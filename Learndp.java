827e1d315906a2475c585042e441e44463115732
Ruturaj Mohanty <kanha95@gmail.com>
2016-10-09 23:19:24 +0530

    lcs top down approach

---
+++ Learndp.java
@@ -0,0 +1,30 @@
+
+import java.util.Arrays;
+
+
+public class Learndp {
+   
+    static int[][] dp;
+    static int check(String s1,String s2,int m,int n){
+        if(m==0 || n==0){
+            return 0;
+        }
+        if(s1.charAt(m-1)==s2.charAt(n-1)){
+            dp[m][n]=check(s1,s2,m-1,n-1)+1;
+            
+        }else{
+            dp[m][n]=Math.max(check(s1,s2,m-1,n),check(s1,s2,m,n-1));
+            
+        }
+        return dp[m][n];
+    } 
+    public static void main(String[] args) {
+       String s1="abcdefg";
+       String s2="bcdggfg";
+       dp=new int[s1.length()+1][s2.length()+1];
+     //  dp[s1.length()][s2.length()]=0;
+       System.out.println(check(s1,s2,s1.length(),s2.length()));
+       // System.out.println(dp[0][0]);
+    }
+    
+}
